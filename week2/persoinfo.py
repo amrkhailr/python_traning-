502f9bc6c2811e25a14c947b3caabf54d4f64ab9
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-04 20:57:11 -0600

    add the following line

---
+++ week2/persoinfo.py
@@ -0,0 +1,16 @@
+Name=input('Name : ')
+Address=input('Address : ')
+City=input('City : ')
+State=input('State : ')
+ZIP=input('ZIP : ')
+Telephone=input('Telephone Number : ')
+print(" My Identity")
+print("==========")
+print('Name', Name)
+print('address',Address)
+print('City: ',City)
+print('Sate: ',State)
+print('Zip:',ZIP)
+print("Phone:",Telephone)
+
+
