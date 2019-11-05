19e1e4327b13f423c856e61002d3f9e4657ed421
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-05 16:05:30 -0600

    add the line

---
+++ week2/exo4.py
@@ -0,0 +1,9 @@
+Percentage = 0.23
+Projectedamount=float(input("What is your projected amount of sales :"))
+profit = Projectedamount * Percentage
+print('Your Profit is: ', profit)
+
+Acre = 43560
+Totaltrack=int(input("What is your total squar feet : "))
+Calculatedamountofacres= Acre / Totaltrack
+print('Your TatalSquarFeet will be',Calculatedamountofacres)
