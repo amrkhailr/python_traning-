747113cfc58c92e8efcb249acec2ccb36b017082
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-05 17:58:11 -0600

    good one

--- week2/exo4.py
+++ week2/exo4.py
@@ -7,3 +7,15 @@ Acre = 43560
 Totaltrack=int(input("What is your total squar feet : "))
 Calculatedamountofacres= Acre / Totaltrack
 print('Your TatalSquarFeet will be',Calculatedamountofacres)
+
+item1 = float(input('Item1 price'))
+item2 = float(input('item2 price'))
+item3 = float(input('item3 proce'))
+item4 = float(input('item4 price'))
+item5 = float(input('item5 price'))
+totaleitems = item1 + item2 + item3 + item4 + item5
+taxsales = totalitems  * 0.06
+subtotal = totalitems + taxsales 
+
+print('Total item prices with no TAX', totalitems)
+print('')
