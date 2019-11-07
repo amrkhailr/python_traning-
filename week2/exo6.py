b16bba6e093fca770e2c0ca3ce9ffdec30f16912
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-07 01:32:01 -0600

    Distance

---
+++ week2/exo6.py
@@ -0,0 +1,9 @@
+Speed = 60 # mph 
+
+Distance1 = 5 * 60 # mph
+Distance2 = 8 * 60 # mph
+Distance3 = 12 * 60 # mph
+
+print('The distance the car will travel in 5 hours at 60 mile is' ,Distance1)
+print('The distance the car will travel in 8 hours at 60 mile is' ,Distance2)
+print('The distance the car will travel in 12 hours at 60 mile is' ,Distance3)
