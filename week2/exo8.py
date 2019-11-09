44298e5b33c28048a316dce05481335fc85755b1
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-09 16:32:37 -0600

    Miles-per-Gallon

---
+++ week2/exo8.py
@@ -0,0 +1,11 @@
+ #7. Miles-per-Gallon
+# # A car’s miles-per-gallon (MPG) can be calculated with the following formula:
+#MPG = Miles driven / Gallons of gas used
+#Write a program that asks the user for the number of miles driven and the gallons of gas used. It should calculate the car’s MPG and display the result.
+
+Traveledmiles = float(input('How many miles did you travel? '))
+Gas = float(input('How much gallon Gas did you spend? '))
+
+MPG = Traveledmiles / Gas
+print('Your total MPG is ', MPG)
+
