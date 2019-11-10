f0bd9144b8759aee7224f54f1ecdba8c1659ec33
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-10 11:55:39 -0600

    Temperature Converter

---
+++ week2/exo10.py
@@ -0,0 +1,11 @@
+#Celsius to Fahrenheit Temperature Converter
+#Write a program that converts Celsius temperatures to Fahrenheit temperatures. The formula is as follows: 
+#F=(9/5)*C + 32
+#The program should ask the user to enter a temperature in Celsius, and then display the
+#temperature converted to Fahrenheit.
+
+
+C = float(input('Please enter the temperature in Celsius : '))
+F=(9/5)*C + 32
+print('Your temperature is', F ,'Fahrenheit')
+
