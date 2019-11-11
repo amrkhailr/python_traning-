ed032fe74e2ccff3da30a2947dee37d7b0c4bf7e
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-11 00:11:34 -0600

    Percentage

---
+++ week3/exo5.py
@@ -0,0 +1,13 @@
+#3. How Much Insurance?
+#Many financial experts advise that property owners should insure their homes or buildings for at least 80 percent of the amount it would cost to replace the structure. 
+#Write a program that asks the user to enter the replacement cost of a building and then displays the minimum amount of insurance he or she should buy for the property.
+
+def insurance(Amount):
+    percent = 0.8
+    print(percent*Amount)
+    
+
+Money = float(input('How much you want to put'))
+insurance(Money)
+
+
