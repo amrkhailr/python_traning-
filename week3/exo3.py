9c537395ef2ab8bafed5d71d0db2177342c55ce6
Romal Amarkhail <ramarkhail@apogee.us>
2019-11-10 21:22:09 -0600

    km_to_mile

---
+++ week3/exo3.py
@@ -0,0 +1,8 @@
+#Write a program that asks the user to enter a distance in kilometers, and then converts that distance to miles. The conversion formula is as follows:
+#Miles = Kilometers x 0.6214
+
+def my_function(Kilometers):
+    Miles = Kilometers * 0.6214
+    print('You did',Miles)
+
+my_function(60)
