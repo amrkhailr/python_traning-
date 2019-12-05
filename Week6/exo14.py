c026dd8b9fd0da89f1ed7028bd69c315796ca0b0
Romal Amarkhail <ramarkhail@apogee.us>
2019-12-04 22:06:10 -0600

    add

---
+++ Week6/exo14.py
@@ -0,0 +1,4 @@
+#A file exists on the disk named students.txt. The file contains several records, 
+# and each record contains two fields: (1) the student’s name, and (2) the student’s score for the final exam. 
+# Write code that deletes the record containing “John Perz” as the student name.
+
